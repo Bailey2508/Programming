2708e5d05853b0d3ba20f245a8ef9e26c6575016
Bailey Cook <c3357040@uon.edu.au>
2020-02-27 09:49:36 +1100

    second commit

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main()
     float x;
     float y;
     printf("enter a number: ");
-    scanf("%f", &x);
+    scanf("%f", &x); // add comments to code
     y = x+2/x-1 ;
     printf("y: %f\n", y);
     return 0;
